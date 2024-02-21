977a756262ee3b60f9ff0e0e1ffe37b418274b02
ingjeon <minjae77777@gmail.com>
2024-02-21 15:55:14 +0900

    3

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -11,7 +11,7 @@
                 </li>
                 <li class="header-icon-list">
                     <router-link :to="{ path: '/webtoon', query: { classify: true } }" class="header-icon-list-link">
-                        웹툰 ㄱㄱ2
+                        웹툰 ㄱㄱ3
                     </router-link>
                 </li>
                 <li class="header-icon-list">
