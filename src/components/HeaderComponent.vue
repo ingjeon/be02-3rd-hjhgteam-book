416dc8d1cac1c2772bbf037689f1c4c65fb208fe
ingjeon <minjae77777@gmail.com>
2024-02-21 14:50:53 +0900

    1111

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -11,7 +11,7 @@
                 </li>
                 <li class="header-icon-list">
                     <router-link :to="{ path: '/webtoon', query: { classify: true } }" class="header-icon-list-link">
-                        웹114
+                        웹1111
                     </router-link>
                 </li>
                 <li class="header-icon-list">
